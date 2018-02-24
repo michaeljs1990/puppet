208798aee752551ac7e00439039a099319c7c0e8
Michael Schuett <michaeljs1990@gmail.com>
2018-02-24 19:17:11 +0000

    Initial Commit
    This is just a bunch of template code while trying to figure out
    how the heira structure should be setup.

---
+++ manifests/hiera_test.pp
@@ -0,0 +1,19 @@
+# /etc/puppetlabs/code/environments/production/modules/profile/manifests/hiera_test.pp
+class profile::hiera_test (
+  Boolean             $ssl,
+  Boolean             $backups_enabled,
+  Optional[String[1]] $site_alias = undef,
+) {
+  file { '/tmp/hiera_test.txt':
+    ensure  => file,
+    content => @("END"),
+               Data from profile::hiera_test
+               -----
+               profile::hiera_test::ssl: ${ssl}
+               profile::hiera_test::backups_enabled: ${backups_enabled}
+               profile::hiera_test::site_alias: ${site_alias}
+               |END
+    owner   => root,
+    mode    => '0644',
+  }
+}
