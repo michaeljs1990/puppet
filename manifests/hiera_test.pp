410cf5daa947b5cc14cd8b8f120dbc60657856bb
Michael Schuett <michaeljs1990@gmail.com>
2018-02-25 02:49:09 +0000

    Hiera now with collins
    Use collins facts to define the hiera hierarchy.

--- manifests/hiera_test.pp
+++ manifests/hiera_test.pp
@@ -3,6 +3,8 @@ class profile::hiera_test (
   Boolean             $ssl,
   Boolean             $backups_enabled,
   Optional[String[1]] $site_alias = undef,
+  Optional[String[1]] $nodeclass = undef,
+  Optional[String[1]] $pool = undef,
 ) {
   file { '/tmp/hiera_test.txt':
     ensure  => file,
@@ -12,6 +14,8 @@ class profile::hiera_test (
                profile::hiera_test::ssl: ${ssl}
                profile::hiera_test::backups_enabled: ${backups_enabled}
                profile::hiera_test::site_alias: ${site_alias}
+               profile::hiera_test::nodeclass: ${nodeclass}
+               profile::hiera_test::pool: ${pool}
                |END
     owner   => root,
     mode    => '0644',
