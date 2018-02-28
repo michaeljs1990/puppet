6087a3924547e443197d614095bcfed4b1dea429
Michael Schuett <michaeljs1990@gmail.com>
2018-02-28 04:30:11 +0000

    Cleanup and improved vault modules
    Cleaned up a bunch of crap in here and added a Puppetfile.
    The start of adding a module to download consul-template
    since the open source one isn't super great and generates
    some real shit looking configs.

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,11 +9,11 @@ $nodeclass = $facts['collins']['nodeclass']
 # Nothing else works unless this is run in this repo unless
 # you manage to not need any password management at all.
 # include vault::consul_template
-include vault::consul_template
+include vault
 
 # Now you can do everything you would expect
 include base
 include $site
 include $nodeclass
 
-Class[vault::consul_template] -> Class[base] -> Class[$site] -> Class[$nodeclass]
+Class[vault] -> Class[base] -> Class[$site] -> Class[$nodeclass]
