af1be593b0e13fecaaea2e50df7fbe2346cc499e
Michael Schuett <michaeljs1990@gmail.com>
2018-03-18 07:05:56 +0000

    Order classes
    Order classes in manifest to try and keep logic super specific to
    my environment out of the modules... Although in order to use this
    right now you have to have collins setup and run bionic so I don't
    think this will help anyone. Will try and clean more of this up
    later so it's no so tied into my setup.

--- manifests/nodeclass/devnode.pp
+++ manifests/nodeclass/devnode.pp
@@ -1,5 +1,11 @@
 class devnode {
 
+  Class[mounts::media] -> Class[sickrage]
+  Class[mounts::backup] -> Class[sickrage]
+
+  Class[mounts::media] -> Class[plex]
+  Class[mounts::backup] -> Class[plex]
+
   # testing out plex for now
   include mounts::media
   include mounts::backup
