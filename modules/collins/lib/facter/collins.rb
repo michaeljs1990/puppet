f640167194ac6619604aed7f23eef391dd7f3e5f
Michael Schuett <michaeljs1990@gmail.com>
2018-04-16 03:32:13 +0000

    Pull from disk when collins is down
    Add in the ability for puppet to run without any external resources
    outside of vault. Will add the ability to try and run when vault is down
    in the future.

--- modules/collins/lib/facter/collins.rb
+++ modules/collins/lib/facter/collins.rb
@@ -1,4 +1,6 @@
 require 'collins_auth'
+require 'json'
+require 'ostruct'
 require 'socket'
 
 # Collins facter is fairly strict as far as not letting you grab
@@ -13,12 +15,32 @@ require 'socket'
 Facter.add(:collins) do
   setcode do
     collins_hash = {}
+    static_file = "/etc/collins.json"
 
     # Create a client from COLLINS_CLIENT_CONFIG, ~/.collins.yml, 
     # /etc/collins.yml, or /var/db/collins.yml
-    hostname = Socket.gethostname
-    client = Collins::Authenticator.setup_client
-    asset = (client.find hostname: hostname).first
+    begin
+      hostname = Socket.gethostname
+      Facter.debug "Pulling asset from collins remote DB using: #{hostname}"
+      client = Collins::Authenticator.setup_client
+      asset = (client.find hostname: hostname).first
+    rescue => ex
+      # This is an expected condition but leave this here to easily turn on logging
+      # Facter.log_exception ex
+      Facter.warn "Collins lookup failed. pulling asset from disk: #{static_file}"
+
+      asset = nil
+      File.open(static_file, "r") do |f|
+        json_hash = JSON.parse(f.read)
+        # Cleanup to make the JSON look like collins returned it or in other
+        # words move asset.state to asset.state.label and make this feel like
+        # an object. This can likely be done a nicer way but it escapes me at
+        # this time sorry :(.
+        asset = OpenStruct.new(json_hash) 
+        state = asset.state
+        asset.state = OpenStruct.new(:label => state)
+      end
+   end
 
     # determine the site from the hostname
     site = asset.hostname.split('.').at 1
@@ -36,6 +58,15 @@ Facter.add(:collins) do
       :rack_position  => asset.rack_position,
       :secondary_role => asset.secondary_role
     }
+
+    begin
+      # Write the above collins info out to a flat file for future use.
+      File.open(static_file, "w") do |f|
+        f.write(JSON.pretty_generate(collins_hash))
+      end
+    rescue => ex
+      Facter.log_exception ex
+    end
 
     collins_hash
   end
